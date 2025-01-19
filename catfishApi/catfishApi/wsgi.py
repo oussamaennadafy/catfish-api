0f78520ac8bfccfe927513a0c5bac2ea88f53d61
oussama ennadafy <oussama.ennadafy@gmail.com>
2025-01-19 11:45:32 +0100

    change app name

--- catfishApi/catfishApi/wsgi.py
+++ catfishApi/catfishApi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for catfishDjango project.
+WSGI config for catfishApi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'catfishDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'catfishApi.settings')
 
 application = get_wsgi_application()
--- catfishApi/catfishApp/apps.py
+++ catfishApi/catfishApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CatfishdjangoappConfig(AppConfig):
+class catfishApiappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'catfishDjangoApp'
+    name = 'catfishApiApp'
