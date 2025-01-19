5f39d03150a2ea6e44c6dab15d9c17decd9368cd
oussama ennadafy <oussama.ennadafy@gmail.com>
2025-01-19 11:48:52 +0100

    fix bugs after changing app name

--- catfishApi/catfishApp/apps.py
+++ catfishApi/catfishApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class catfishApiappConfig(AppConfig):
+class catfishappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'catfishApiApp'
+    name = 'catfishApp'
