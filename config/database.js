e746c27715d388a99991abe0ede6361b518e85d7
oussama ennadafy <oussama.ennadafy@gmail.com>
2025-02-21 23:01:54 +0100

    update database configuration for AWS RDS

--- config/database.js
+++ config/database.js
@@ -1,6 +1,16 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('catfish', 'postgres', 'postgres', {
-  host: 'localhost',
+export const sequelize = new Sequelize({
+  database: "catfish_db",
+  username: "postgres",
+  password: "oussama.aws.rds.catfish",
+  host: "catfish.cmd8yamys2yk.us-east-1.rds.amazonaws.com",
+  port: 5432,
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
+  },
 });
