ab765b2739fffa2eaff102435d8fd3f86e415ec7
oussama ennadafy <oussama.ennadafy@gmail.com>
2025-03-09 12:06:49 +0000

    remove unnecessary blank line from rsync script

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,15 +1,12 @@
-
 # sync local files with ec2 instance
 rsync -avz --exclude 'node_modules' --exclude '.git' --exclude '.env' \
 -e "ssh -i ~/Desktop/casablanca-oussama-mac.pem" \
 . ubuntu@ec2-18-212-206-197.compute-1.amazonaws.com:~/app
 
-
 # connect to ec2 instance
 cd ..
 chmod 400 "casablanca-oussama-mac.pem"
 ssh -i "casablanca-oussama-mac.pem" ubuntu@ec2-18-212-206-197.compute-1.amazonaws.com
-
 
 # caddy file
 sudo vim /etc/caddy/Caddyfile
