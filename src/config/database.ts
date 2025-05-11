c302fd95de5cf95ac42b155d75a2102df6e6da86
oussama ennadafy <oussama.ennadafy@gmail.com>
2025-05-11 21:12:16 +0100

    refactor: update DB_URL construction to conditionally include sslmode

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,6 +6,8 @@ const DATABASE_PASSWORD = process.env.NODE_ENV === "production" ? process.env.PR
 const DATABASE_HOST = process.env.NODE_ENV === "production" ? process.env.PROD_DATABASE_HOST : process.env.DEV_DATABASE_HOST;
 const DATABASE_PORT = process.env.NODE_ENV === "production" ? process.env.PROD_DATABASE_PORT : process.env.DEV_DATABASE_PORT;
 
-export const DB_URL = `postgres://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}?sslmode=no-verify`;
+const sslmode =  process.env.NODE_ENV === "production" ? "sslmode=no-verify" : "";
+
+export const DB_URL = `postgres://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}?${sslmode}`;
 
 export const db = drizzle(DB_URL);
