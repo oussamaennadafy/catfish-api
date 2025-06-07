addee691bd10e642e62cbe8bed1e92734c8a8046
oussama ennadafy <oussama.ennadafy@gmail.com>
2025-06-07 01:31:49 +0100

    add chat feature

--- src/events/handlers/chatHandler.ts
+++ src/events/handlers/chatHandler.ts
@@ -1,17 +1,14 @@
+import { ChatEvents } from '@/features/chat/constants/events.ts';
+import { Message } from '@/features/chat/types/index.ts';
 import { Socket } from 'socket.io';
 
 export default class ChatHandler {
   
   public handleConnection(socket: Socket): void {
-    // Chat-related socket events
-    socket.on('send-message', async (message: any) => {
-      // Message sending logic here
+    socket.on(ChatEvents.client.SEND_MESSAGE, async (message: Message) => {
+      const roomID = socket.data["roomID"];
+      socket.to(roomID).emit(ChatEvents.server.RECEIVE_MESSAGE, message);
+      socket.emit(ChatEvents.server.RECEIVE_MESSAGE, message);
     });
-    
-    socket.on('typing', (roomId: string, userId: string) => {
-      socket.to(roomId).emit('user-typing', userId);
-    });
-    
-    // Other chat-related events...
   }
 }
