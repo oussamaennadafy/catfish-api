84b58c2b047de3886fd32831937cee41a26e3921
oussama ennadafy <oussama.ennadafy@gmail.com>
2025-06-11 15:09:35 +0100

    feat: implement toggle camera and mic functionality in room events, and refactore chat controller

--- src/events/handlers/chatHandler.ts
+++ src/events/handlers/chatHandler.ts
@@ -3,12 +3,21 @@ import { Message } from '@/features/chat/types/index.ts';
 import { Socket } from 'socket.io';
 
 export default class ChatHandler {
-  
+
   public handleConnection(socket: Socket): void {
-    socket.on(ChatEvents.client.SEND_MESSAGE, async (message: Message) => {
-      const roomID = socket.data["roomID"];
-      socket.to(roomID).emit(ChatEvents.server.RECEIVE_MESSAGE, message);
-      socket.emit(ChatEvents.server.RECEIVE_MESSAGE, message);
-    });
+    // when user send message
+    socket.on(ChatEvents.client.SEND_MESSAGE, async (message: Message) => this.sendMessageHandler(socket, message));
+  }
+
+  /**
+   * @description this function handle all logic of join room event
+   * @param socket 
+   * @param message
+   * @returns Promise<void>
+   */
+  private async sendMessageHandler(socket: Socket, message: Message) {
+    const roomID = socket.data["roomID"];
+    socket.to(roomID).emit(ChatEvents.server.RECEIVE_MESSAGE, message);
+    socket.emit(ChatEvents.server.RECEIVE_MESSAGE, message);
   }
 }
--- src/features/rooms/controllers/roomController.ts
+++ src/features/rooms/controllers/roomController.ts
@@ -12,19 +12,24 @@ export default class RoomHandler {
   io: SocketIOServer;
 
   public async handleConnection(socket: Socket) {
-    // Listen if a user joins a room
+    // when users join room
     socket.on(RoomEvents.client.JOIN_ROOM, async (userId: string) => this.joinRoomHandler(socket, userId));
 
     // when users leave room
-    socket.on(RoomEvents.client.LEAVE_ROOM, async (userId: string) => RoomHandler.leaveRoomHandler(socket));
+    socket.on(RoomEvents.client.LEAVE_ROOM, async () => RoomHandler.leaveRoomHandler(socket));
+
+    // when user toggle camera
+    socket.on(RoomEvents.client.TOGGLE_CAMERA, async (userId: string, alsoEmitToMe: boolean) => this.ToogleCameraHandler(socket, userId, alsoEmitToMe));
+
+    // when user toggle mic
+    socket.on(RoomEvents.client.TOGGLE_MIC, async (userId: string, alsoEmitToMe: boolean) => this.ToogleMicHandler(socket, userId, alsoEmitToMe));
   }
 
   /**
-   * 
    * @description this function handle all logic of join room event
    * @param socket 
-   * @param roomType
-   * @returns void
+   * @param userId
+   * @returns Promise<void>
    */
   private async joinRoomHandler(socket: Socket, userId: string) {
     const availableRooms = await db.select().from(roomModel).where(eq(roomModel.isFull, false));
@@ -53,25 +58,55 @@ export default class RoomHandler {
   }
 
   /**
-   * 
    * @description this function hanlde all logic of leave room event
    * @param socket
-   * @returns void
+   * @returns Promise<void>
   */
   static async leaveRoomHandler(socket: Socket) {
     const rooms = await db.select().from(roomModel).where(eq(roomModel.id, socket.data["roomID"]));
     if (rooms.length === 0) return;
     const room = rooms.at(0);
-    if(room.membersCount === 2) {
+    if (room.membersCount === 2) {
       await db.update(roomModel).set({
         isFull: false,
         membersCount: 1,
       }).where(eq(roomModel.id, room.id));
-    } else if(room.membersCount === 1) {
+    } else if (room.membersCount === 1) {
       await db.delete(roomModel).where(eq(roomModel.id, room.id));
     }
     await socket.leave(room.id.toString());
     socket.emit(RoomEvents.server.READY_TO_JOIN);
+  }
+
+  /**
+   * @description this function hanlde all logic of toggle camera room event
+   * @param socket
+   * @param userId
+   * @param alsoEmitToMe
+   * @returns Promise<void>
+  */
+  private async ToogleCameraHandler(socket: Socket, userId: string, alsoEmitToMe = true) {
+    const roomID = socket.data["roomID"];
+    socket.to(roomID).emit(RoomEvents.server.CAMERA_TOGGLED, userId);
+    if (alsoEmitToMe) {
+      socket.emit(RoomEvents.server.CAMERA_TOGGLED, userId);
+    }
+  }
+
+
+  /**
+   * @description this function hanlde all logic of toggle camera room event
+   * @param socket
+   * @param userId
+   * @param alsoEmitToMe
+   * @returns Promise<void>
+  */
+  private async ToogleMicHandler(socket: Socket, userId: string, alsoEmitToMe = true) {
+    const roomID = socket.data["roomID"];
+    socket.to(roomID).emit(RoomEvents.server.MIC_TOGGLED, userId);
+    if (alsoEmitToMe) {
+      socket.emit(RoomEvents.server.MIC_TOGGLED, userId);
+    }
   }
 
 }
