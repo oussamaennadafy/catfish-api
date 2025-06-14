55067656b68ebdc9661c02e3895a0d3a1de13744
oussama ennadafy <oussama.ennadafy@gmail.com>
2025-06-14 21:47:44 +0100

    fix: update EC2 instance address in rsync script

--- src/scripts/rsync.sh
+++ src/scripts/rsync.sh
@@ -1,12 +1,12 @@
 # sync local files with ec2 instance
 rsync -avz --exclude 'node_modules' --exclude '.git' --exclude '.env' --exclude 'dist' \
 -e "ssh -i ~/Desktop/casablanca-oussama-mac.pem" \
-. ubuntu@ec2-18-212-206-197.compute-1.amazonaws.com:~/app
+. ubuntu@ec2-44-204-42-93.compute-1.amazonaws.com:~/app
 
 # connect to ec2 instance
 cd ..
 chmod 400 "casablanca-oussama-mac.pem"
-ssh -i "casablanca-oussama-mac.pem" ubuntu@ec2-18-212-206-197.compute-1.amazonaws.com
+ssh -i "casablanca-oussama-mac.pem" ubuntu@ec2-44-204-42-93.compute-1.amazonaws.com
 
 # restart background service
 sudo systemctl restart myapp.service
